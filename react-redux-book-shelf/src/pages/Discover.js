5244114efb8148a70fd804266beffe18f5f06f8b
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-25 21:20:47 +0600

    [ADD] react-redux-book-shelf

---
+++ react-redux-book-shelf/src/pages/Discover.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Book from '../components/Book/Book';
+// import books from '../fakeData/books.json'
+import PageLayout from '../components/PageLayout/PageLayout';
+const Discover = () => {
+    const books = useSelector((state) => {
+        return state.books.discoverList
+    })
+    
+    return (
+        <PageLayout>
+            {
+                books?.map((book) => (<Book key={book.id} book={book} />))
+            }
+        </PageLayout>
+    );
+};
+
+export default Discover;
