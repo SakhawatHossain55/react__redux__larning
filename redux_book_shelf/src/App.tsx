ee5facf7f18264eadaed06e3e28e45ac33e506b7
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-29 21:52:46 +0600

    [Redux_book_shelf] freeCodecamp

---
+++ redux_book_shelf/src/App.tsx
@@ -0,0 +1,69 @@
+
+import { useQuery } from 'react-query';
+// Components
+import Item from './Item/Item';
+// import Cart from './Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper, StyledButton } from './App.styles';
+import { useState } from 'react';
+//Types 
+export interface CartItemType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  amount: number;
+}
+
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json()
+
+function App() {
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data);
+
+  const getTotalItems = (items: CartItemType[]) => 
+
+  items.reduce((ack: number, item) => ack + item.amount, 0)
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />
+
+  if (error) return <div>Something went wrong.....</div>
+
+  return (
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {
+          data?.map(item => (
+            <Grid item key={item.id} xs={12} sm={4}>
+              <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Wrapper>
+  );
+}
+
+export default App;
